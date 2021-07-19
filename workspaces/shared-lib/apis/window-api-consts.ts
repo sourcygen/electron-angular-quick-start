4fa299996145deb61e3b65d3d05faab0bd8a25e1
Amadou Ada DIENE <aa.diene@hotmail.fr>
2021-07-19 12:34:50 +0200

    feat: migrate from electron-webpack to electron-forge
    Electron upgrade : 10.1.3 => 13.1.7
    Angular upgrade : 10.1.3 => 12.1.2

---
+++ workspaces/shared-lib/apis/window-api-consts.ts
@@ -0,0 +1,17 @@
+export class WindowApiConst {
+  /** Channel used by the renderer process to send data to the main process */
+  public static readonly MULTIPLES_INPUT = "getMultiplesInput";
+
+  /** Channel used by the renderer process to receive data from the main process */
+  public static readonly MULTIPLES_OUTPUT = "getMultiplesOutput";
+
+  /** Whitelist of the safe channels to use when sending data to the main process */
+  public static readonly SENDING_SAFE_CHANNELS = [
+    WindowApiConst.MULTIPLES_INPUT,
+  ];
+
+  /** Whitelist of the safe channels to use when receiving data from the main process */
+  public static readonly RECEIVING_SAFE_CHANNELS = [
+    WindowApiConst.MULTIPLES_OUTPUT,
+  ];
+}
