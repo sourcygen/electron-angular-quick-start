08e266d3424e2de6e88208e7a91679005eb1aea6
Amadou A. DIENE <aa.diene@hotmail.fr>
2023-04-22 22:23:24 +0200

    chore(deps): upgrading angular from 14.2.3 to 15.2.8

--- workspaces/angular-app/src/app/components/multiples/multiples.component.spec.ts
+++ workspaces/angular-app/src/app/components/multiples/multiples.component.spec.ts
@@ -1,14 +1,40 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { MultiplesComponent } from './multiples.component';
+import { ElectronIpcService } from 'src/app/services/electron-ipc.service';
+import {
+	TranslateFakeLoader,
+	TranslateLoader,
+	TranslateModule,
+	TranslateService,
+} from '@ngx-translate/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 describe('MultiplesComponent', () => {
-	let component: MultiplesComponent;
+	const mockElectronIpcService = jasmine.createSpyObj(['receive', 'send']);
 	let fixture: ComponentFixture<MultiplesComponent>;
+	let component: MultiplesComponent;
 
 	beforeEach(waitForAsync(() => {
 		TestBed.configureTestingModule({
 			declarations: [MultiplesComponent],
+			imports: [
+				ReactiveFormsModule,
+				TranslateModule,
+				TranslateModule.forRoot({
+					loader: {
+						provide: TranslateLoader,
+						useClass: TranslateFakeLoader,
+					},
+				}),
+			],
+			providers: [
+				TranslateService,
+				{
+					provide: ElectronIpcService,
+					useValue: mockElectronIpcService,
+				},
+			],
 		}).compileComponents();
 	}));
 
